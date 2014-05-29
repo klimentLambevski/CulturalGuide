96c6802e80fff8a6d45897c150799a753104de28
Kliment Lambevski <kliment@anas-MacBook-Air.local>
2014-05-29 11:26:49 +0200

    Od novo

---
+++ CulturalGuide/klcViewController.h
@@ -0,0 +1,13 @@
+//
+//  klcViewController.h
+//  CulturalGuide
+//
+//  Created by Kliment Lambevski on 5/29/14.
+//  Copyright (c) 2014 Kliment Lambevski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface klcViewController : UIViewController
+
+@end
